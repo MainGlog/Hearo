cafc785853724f7b3d5adb3dd745d62fa754f157
lbrooks81 <143453730+lbrooks81@users.noreply.github.com>
2025-05-22 13:04:18 -0400

    Removed the default index header
    Began developing progress bar
    Thanks https://www.youtube.com/watch?v=J95MC2Koymc
    
    Developed a countdown timer

--- application/app/Training.tsx
+++ application/app/Training.tsx
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet, Animated, Button} from "react-native";
+import {View, Text, StyleSheet, Animated, Button, TextInput} from "react-native";
 import Routine from "@/models/Routine";
 import ScaleExercise from "@/models/ScaleExercise";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
@@ -41,62 +41,30 @@ export default function TrainingScreen({route}: TrainingScreenProps){
     }
 
     const [exercisesPlayed, setExercisesPlayed] = useState<number>(0);
+    const [guess, setGuess] = useState<string>('');
     // TODO for each exercise, increment exercisesPlayed by one
-    const Progress = ({step, steps, height}: {step: number; steps: number; height: number}) => {
-        const [width, setWidth] = useState<number>(0);
-        const animatedValue = useRef(new Animated.Value(-1000)).current; // We set it to -1000 so the value is outside the screen before getting the width of the container
-        const reactive = useRef(new Animated.Value(-1000)).current; // useRef allows the value to remain the same after re-renders, similar to remember in Kotlin
-
-        useEffect(() => {
-            reactive.setValue(-width + (width * step) / steps);
-
-            Animated.timing(animatedValue, {
-                toValue: reactive,
-                duration: 100,
-                useNativeDriver: true,
-            }).start();
-        }, [step, width]);
-
-        return (
-            <>
-                <Text style={styles.progressText}>{step} / {steps}</Text>
-                <View
-                    onLayout={e => {
-                        setWidth(e.nativeEvent.layout.width);
-                    }}
-                    style={{
-                        height,
-                        backgroundColor: 'rgba(0,0,0,0.1)',
-                        borderRadius: height,
-                        overflow: 'hidden'
-                    }}
-                >
-                    <Animated.View
-                        style={{
-                            height,
-                            width: '100%',
-                            borderRadius: height,
-                            backgroundColor: 'rgba(0,0,0,0.5)',
-                            transform: [{
-                                translateX: animatedValue
-                            }]
-                        }}
-                    />
-                </View>
-            </>
-        )
-    }
 
     return (
         <View style={styles.container}>
             <StatusBar hidden/>
-            <Progress step={exercisesPlayed} steps={10} height={20}/>
+            <Progress step={exercisesPlayed} steps={routine.exerciseCount} height={20}/>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Text style={styles.topBarText}>{exercisesPlayed} / {routine.exerciseCount}</Text>
+                <Timer duration={routine.timeToGuess}/>
+            </View>
 
             <Button title={"Increment Step"} onPress={() => setExercisesPlayed(exercisesPlayed + 1)}/>
             <Button title={"Reset Step"} onPress={() => setExercisesPlayed(0)}/>
+            <TextInput
+                style={styles.guessInput}
+                value={guess}
+                onChangeText={(text) => {
+                    setGuess(text);
+                }}
+                placeholder={'Enter your Guess'}
+            />
         </View>
     );
-
 }
 
 function getRandomExercise(routine: Routine): ScaleExercise | ChordExercise | NotesExercise  {
@@ -114,20 +82,103 @@ function getRandomExercise(routine: Routine): ScaleExercise | ChordExercise | No
     return exercise;
 }
 
+const Progress = ({step, steps, height}: {step: number; steps: number; height: number}) => {
+    const [width, setWidth] = useState<number>(0);
+    const animatedValue = useRef(new Animated.Value(-1000)).current; // We set it to -1000 so the value is outside the screen before getting the width of the container
+    const reactive = useRef(new Animated.Value(-1000)).current; // useRef allows the value to remain the same after re-renders, similar to remember in Kotlin
+
+    useEffect(() => {
+        reactive.setValue(-width + (width * step) / steps);
+
+        Animated.timing(animatedValue, {
+            toValue: reactive,
+            duration: 100,
+            useNativeDriver: true,
+        }).start();
+    }, [step, width]);
+
+    return (
+        <View
+            onLayout={e => {
+                setWidth(e.nativeEvent.layout.width);
+            }}
+            style={{
+                height,
+                backgroundColor: 'rgba(0,0,0,0.1)',
+                borderRadius: height,
+                overflow: 'hidden',
+                marginBottom: 25
+            }}
+        >
+            <Animated.View
+                style={{
+                    height,
+                    width: '100%',
+                    borderRadius: height,
+                    backgroundColor: 'rgba(0,0,0,0.5)',
+                    transform: [{
+                        translateX: animatedValue
+                    }]
+                }}
+            />
+        </View>
+    )
+}
+
+const Timer = ({duration}: {duration: number}) => {
+    const [seconds, setSeconds] = useState<number>(duration);
+    const [minutes, setMinutes] = useState<number>(Math.floor(duration / 60));
+
+    // Sets a reference for an interval timeout to be used when clearing the interval
+    const intervalRef = useRef<NodeJS.Timeout>();
+
+    useEffect(() => {
+        intervalRef.current = setInterval(() => {
+            setSeconds(prev => {
+                // Stops the interval when seconds reaches 0
+                if (prev <= 1) {
+                    clearInterval(intervalRef.current);
+                    return 0;
+                }
+                return prev - 1;
+            });
+            if (seconds >= 60 && seconds % 60 === 0) {
+                setMinutes(prev => prev - 1);
+            }
+        }, 1000);
+
+        return () => clearInterval(intervalRef.current);
+    }, [seconds]);
+
+    return(
+        <Text style={styles.topBarText}>{addZero(minutes)}:{addZero(seconds % 60)}</Text>
+    )
+}
+
+function addZero(time: number): string {
+    return time.toString().length < 2 ? `0${time}` : `${time}`;
+}
+
 const styles = StyleSheet.create({
     topBar: {
-        backgroundColor: '#7f926d',
         height: 50
     },
-    progressText: {
-        fontSize: 12,
+    topBarText: {
+        fontSize: 16,
         fontWeight: 900,
-        marginBottom: 5
+        marginBottom: 5,
+        marginHorizontal: 10
     },
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         justifyContent: 'center',
-        padding: 20
+        position: 'absolute',
+        top: 25,
+        left: 20,
+        right: 20,
+    },
+    guessInput: {
+        textAlign: 'center',
+        fontSize: 16,
     }
 })
